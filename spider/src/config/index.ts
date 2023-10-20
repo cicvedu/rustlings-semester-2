b8dfccf4921f46b124d0c9d3c57f8a6a13af8d7c
ZhiyuanSue <108735103+ZhiyuanSue@users.noreply.github.com>
2023-10-20 11:17:02 +0800

    Update index.ts

--- spider/src/config/index.ts
+++ spider/src/config/index.ts
@@ -2,9 +2,9 @@ import * as dotenv from "dotenv";
 
 export const config = dotenv.config();
 
-export const organiztion: string = 'learningOS';            // 组织
-export const fullOrganization: string = '19380377-learning-rust'; // classroom名称
-export const assignment = 'rust-rustlings-2023-autumn';   // assignment
+export const organiztion: string = 'cicvedu';            // 组织
+export const fullOrganization: string = '119796606-cicv-rust-classroom'; // classroom名称
+export const assignment = 'rustlings-semester-2';   // assignment
 export const works = ['main']; // assignment 的不同情况
 export const AUTH_TOKEN = process.env['TOKEN'];
 export const SESSION_TOKEN = process.env['SESSION_TOKEN'];
